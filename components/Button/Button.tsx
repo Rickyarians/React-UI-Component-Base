e17dbd50e809caeed6eb072bd58531b1b7ad744b
Ricky Ariansyah <ricky.sugandi@permatabank.co.id>
2026-04-20 01:16:08 +0700

    init

---
+++ components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import {TouchableOpacity, Text} from 'react-native';
+import {useTheme} from '../../theme/ThemeProvider';
+
+export const Button = ({title}:any) => {
+  const theme = useTheme();
+
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: theme.colors.primary,
+        padding: theme.spacing.md,
+        borderRadius: 8
+      }}>
+      <Text style={{color: '#fff'}}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
---
+++ theme/ThemeProvider.tsx
@@ -0,0 +1,14 @@
+import {createContext, useContext} from 'react';
+import {theme} from './theme';
+
+const ThemeContext = createContext(theme);
+
+export const ThemeProvider = ({children}: any) => {
+  return (
+    <ThemeContext.Provider value={theme}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
